53718264a48ec82180027b2bbfa56e72f8e24fd3
unknown <hibit_at@hotmail.com>
2021-03-13 19:41:57 +0900

    コンスツンツェのミス

--- parameter_plot.py
+++ parameter_plot.py
@@ -2,15 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('growth_rate.csv')
-df = df[df['series']=='TH'] #シリーズを限定するのをオススメします
+df = df[df['series'] == 'TH']  # シリーズを限定するのをオススメします
 
-Axis1 = list(df['MAG'])
-Axis2 = list(df['DEF'])
-col_sex = df['sex'].replace('Male','b').replace('Female','r')
+Axis1 = list(df['POW']+df['TEC'])
+Axis2 = list(df['DEF']+df['SPD'])
+col_sex = df['sex'].replace('Male', 'b').replace('Female', 'r')
 
-fig, ax = plt.subplots(figsize=(10,10), dpi=100)
-ax.scatter(Axis1,Axis2,alpha=0) #空のマーカー
-#下のイテレーションでテキストラベルを貼り付ける
-for i,(name,PC1,PC2,col) in enumerate(zip(df['name'],Axis1,Axis2,col_sex)):
-    ax.annotate(name,(PC1,PC2),c = col,fontname="MS Gothic")
+check = [[0]*201 for _ in range(201)]
+
+fig, ax = plt.subplots(figsize=(10, 10), dpi=100)
+ax.scatter(Axis1, Axis2, alpha=0)  # 空のマーカー
+# 下のイテレーションでテキストラベルを貼り付ける
+for i, (name, PC1, PC2, col) in enumerate(zip(df['name'], Axis1, Axis2, col_sex)):
+    check[PC1][PC2] += 1
+    PC2 -= check[PC1][PC2] - 1
+    ax.annotate(name, (PC1, PC2), c=col, fontname="MS Gothic")
+
 plt.show()
--- pca.py
+++ pca.py
@@ -4,24 +4,25 @@ import matplotlib.pyplot as plt
 import sklearn
 from sklearn.decomposition import PCA
 
-#オリジナルデータを読み込む
+# オリジナルデータを読み込む
 df = pd.read_csv('growth_rate.csv')
+df = df[df['series']=='TH']
 
-#後で性別のカラーラベルに使用するためにbとrに置き換えたものを作成
-col_sex = df['sex'].replace('Male','b').replace('Female','r')
+# 後で性別のカラーラベルに使用するためにbとrに置き換えたものを作成
+col_sex = df['sex'].replace('Male', 'b').replace('Female', 'r')
 
-#ここで主成分分析
+# ここで主成分分析
 pca = PCA()
-feature = pca.fit(df.iloc[:,3:])
-feature = pca.transform(df.iloc[:,3:])
+feature = pca.fit(df.iloc[:, 3:])
+feature = pca.transform(df.iloc[:, 3:])
 
-#寄与率
+# 寄与率
 print(pca.explained_variance_ratio_)
 
-#ユニットの名前を散布図にする
-fig, ax = plt.subplots(figsize=(10,10), dpi=100)
-ax.scatter(feature[:,0],feature[:,1],alpha=0) #空のマーカー
-#下のイテレーションでテキストラベルを貼り付ける
-for i,(name,PC1,PC2,col) in enumerate(zip(df['name'],feature[:,0],feature[:,1],col_sex)):
-    ax.annotate(name,(PC1,PC2),c = col,fontname="MS Gothic")
+# ユニットの名前を散布図にする
+fig, ax = plt.subplots(figsize=(10, 10), dpi=100)
+ax.scatter(feature[:, 0], feature[:, 1], alpha=0)  # 空のマーカー
+# 下のイテレーションでテキストラベルを貼り付ける
+for i, (name, PC1, PC2, col) in enumerate(zip(df['name'], feature[:, 0], feature[:, 1], col_sex)):
+    ax.annotate(name, (PC1, PC2), c=col, fontname="MS Gothic")
 plt.show()
